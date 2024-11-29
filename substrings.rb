3bf32c2717e5d0d5372422d6f00436eda3c5019c
Beno <66329054+Molecule-Cloud@users.noreply.github.com>
2024-11-29 17:35:11 +0000

    Substrings done

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,15 @@
-Hello
+def sub_strings(word, dictionary)
+  hash = Hash.new()
+  lower_words = word.downcase.split(" ")
+  lower_words.each do |lower_word|
+    dictionary.each do |find|
+      lower_find = find.downcase
+      count = lower_word.scan(lower_find).length
+      hash[lower_find] = (hash[lower_find] || 0) + count if count > 0
+    end
+  end
+  hash
+end
+p dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p sub_strings("below", dictionary)
+
